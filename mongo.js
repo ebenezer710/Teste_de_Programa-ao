018eebf9c4407f2a3d3ed97cd19e177c1fbdf5af
ebenezer710 <42673729+ebenezer710@users.noreply.github.com>
2022-03-29 19:22:23 -0300

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,33 +1,33 @@
 const { MongoClient } = require('mongodb');
 const axios = require('axios');
 const myURL = new URL('http://universities.hipolabs.com/search?');
+var teste = []
 const paises = ['argentina', 'brazil', 'chile', 'colombia', 'paraguay', 'peru', 'suriname', 'uruguay']
 
 async function main() {
     const uri = "mongodb+srv://teste01:teste01@cluster0.9jziq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     const client = new MongoClient(uri)
-    for (let i = 0; i < paises.length; i++) {
-        myURL.searchParams.set('country', paises[i]);
 
-        console.log(myURL.href);
+    await paises.map(function (value) {
+        myURL.searchParams.set('country', value);
 
-        await axios(myURL.toString())
+        axios(myURL.toString())
             .then(response => {
-                console.log(typeof response)
-                copyItems = Object.assign(response.data);
-            }
-            ).catch();
-    }
+                copyItems = response.data ;
 
-    try {
-        await client.connect();
-        const result = await client.db("universities").collection("countries").insertMany(copyItems)
-        console.log(result.insertedCount)
-    } catch (e) {
-        console.error(e);
-    }
-    finally {
-        await client.close();
-    }
+                console.log(typeof copyItems)
+                teste.push(Object.fromEntries(copyItems))
+                //console.log(copyItems)
+                //return(teste)
+                
+                client.connect((err, db) => {
+                    //teste = teste;
+                    //onsole.log(typeof teste)
+                    //console.log( teste)
+                    const result = client.db("universities").collection("countries").insertMany(teste)
+                    //console.log(result.insertedCount)
+                });
+            }).catch();
+    })
 }
-main().catch(console.error);
+main().catch();
