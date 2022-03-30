788e1b8a54a79ca63e84275c0d050d54a124102d
ebenezer710 <42673729+ebenezer710@users.noreply.github.com>
2022-03-29 23:53:14 -0300

    FASE 1 CONCLUIDA.

--- mongo.js
+++ mongo.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 const axios = require('axios');
-const myURL = new URL('http://universities.hipolabs.com/search?');
+const myURL = new URL('http://universities.hipolabs.com/search?');//site API
 var teste = []
-const paises = ['argentina', 'brazil', 'chile', 'colombia', 'paraguay', 'peru', 'suriname', 'uruguay']
+const paises = ['argentina', 'brazil', 'chile', 'colombia', 'paraguay', 'peru', 'suriname', 'uruguay']//LISTA DE PAISES
 
 async function main() {
-    const uri = "mongodb+srv://teste01:teste01@cluster0.9jziq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://teste01:teste01@cluster0.9jziq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //CONEXAO COM O BD
     const client = new MongoClient(uri)
 
     await paises.map(function (value) {
@@ -13,25 +13,12 @@ async function main() {
 
         axios(myURL.toString())
             .then(response => {
-                copyItems = Object.assign({}, response.data) ;
-
-                //console.log(typeof copyItems)
-                teste.push(copyItems)
-                //console.log(teste)
-                //return(teste)
+                var teste1 = response.data //PEGA O CONTEUDO DO SITE, MAS COLOCA DENTRO DE OUTRO OBJETO
+                teste1.slice(1,-1) //RETIRA O "[]" DO INICIO E DO FINAL DO CONTEUDO 
                 client.connect((err, db) => {
-                    //teste = Object.assign({}, teste);
-                    
-            
-                    //console.log(teste)
-                    //console.log( teste)
-                    const result = client.db("universities").collection("countries").insertMany(teste)
-                    
-                    console.log(typeof result)
+                    const result = client.db("universities").collection("countries").insertMany(teste1) //ADICIONA O OBJETO NO BD 
                 });
             }).catch();
     })
-
-     
 }
 main().catch(console.error);
